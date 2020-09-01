b66e14bff68ff34ca46d26c48c4260972a08047c
Natan Abreu <guilhermenatanabreu@gmail.com>
2020-09-01 20:58:43 -0300

    Adicionando o arquivo .js com os exercícios da revisão
    Lidando com variáveis, manuseio de Strings, tipos de saídas de dados na linguagem, utilizando incrementos.

---
+++ js/exercicios.js
@@ -0,0 +1,53 @@
+//Variável com o seu sexo
+let sex = "Masculino";
+console.log(sex);
+
+//Variável com sua idade
+let idade = 18;
+console.log(idade);
+
+//Variável com comida sem atribuir valor
+let comidaFav;
+//Atribuindo valor
+comidaFav = "Feijão";
+console.log(comidaFav);
+
+//Variável contendo uma String
+let text = "String!";
+console.log(text);
+
+//Variável com um número dentro da String
+let textTwo = "2002";
+console.log(textTwo);
+
+//Variáveis com nome e sobrenome e somar as duas
+let nome = "Natan";
+let sobNome = "Abreu";
+console.log(nome+" "+sobNome);
+
+//Frase na variável
+let frase = "It´s Time";
+console.log(frase);
+
+//Verificando tipo da variável
+console.log(typeof nome);
+
+//Resultado da seguinte expressão
+let express = 20+5+2/2+20;
+console.log(express);
+
+//Duas expressões que retorne NaN
+let nanOne = "150a"-50;
+console.log(nanOne);
+let nanTwo = "100b"-100;
+console.log(nanTwo);
+
+//Somar string com number
+let str = +"200"+50;
+console.log(str);
+
+//Incrementando número 5
+let incrementOne = 5;
+console.log(incrementOne); //Sem incremento
+console.log(++incrementOne); //Pré
+console.log(incrementOne++); //Pós
